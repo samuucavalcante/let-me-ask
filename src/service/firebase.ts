b6495cda952a86d7adb62a6424eb19a59fd8d042
Samuel Cavalcante <samuel-icm-dantas@hotmail.com>
2021-07-02 03:29:21 -0300

    second commit

---
+++ src/service/firebase.ts
@@ -0,0 +1,23 @@
+import firebase from "firebase/app";
+
+import "firebase/auth";
+import "firebase/database";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_PUBLIC_FIREBASE_APP_ID,
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const auth = firebase.auth();
+const database = firebase.database();
+
+export { firebase, auth, database };
